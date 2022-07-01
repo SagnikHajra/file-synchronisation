b03235da4eafb20eb3939123bbfe0121fcc6bcc8
shajra <hajra.sagnik@gmail.com>
2022-06-30 20:49:27 -0500

    Server can handle multiple clients. Initial Code

--- src/Client.java
+++ src/Client.java
@@ -9,8 +9,21 @@ import java.util.concurrent.Semaphore;
 
 
 public class Client {
+    public boolean reader = true;
+    String localDir = null;
 
     public static void main(String[] args) {
+        String localDir = null;
+        switch(args[0]) {
+            case "1":
+                localDir = Constants.CLIENT_ONE_PATH;
+                break;
+            case "2":
+                localDir = Constants.CLIENT_TWO_PATH;
+                break;
+            default:
+                localDir = Constants.CLIENT_ONE_PATH;
+        }
         try{
             InetAddress serverIp=InetAddress.getByName("localhost");
             Socket tcpSocket = new Socket(serverIp, Constants.SERVER_TCP_PORT);
@@ -20,7 +33,7 @@ public class Client {
             String[] line = "ajdgasdhkjha\nausdhkadkahs\nasdgjagdjasgdjhgas\nasdgjhasgdjahgsd\nahsdgjsgadkaskdjh\nSTOP".split("\n");
             for (String words : line){
                 sender.println(words);
-                // Sleep for 10 seconds
+                // Sleep for 2 seconds
                 try {
                     TimeUnit.SECONDS.sleep(2);
                 } catch (InterruptedException e) {
